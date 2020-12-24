b21c871c9de1e42498d08ffdce78a6261e6222e6
cbeck <cruthenbeck@gmail.com>
2020-12-23 22:09:19 -0500

    Step004: Configure the greeting

--- aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3/Startup.cs
+++ aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,11 @@ namespace aspnet_core_fundamentals_v3
 {
     public class Startup
     {
+        public IConfiguration Config { get; }
+        public Startup(IConfiguration config)
+        {
+            Config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +38,7 @@ namespace aspnet_core_fundamentals_v3
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("You had me at 'hello'!");
+                    await context.Response.WriteAsync(Config["greeting"]);
                 });
             });
         }
