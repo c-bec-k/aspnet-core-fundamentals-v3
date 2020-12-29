3cc723557fc6514999acb5676bbe45a0c0d15eba
cbeck <cruthenbeck@gmail.com>
2020-12-28 20:26:14 -0500

    Step011: mvc attribute routing

--- aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3/Controllers/AboutController.cs
+++ aspnet-core-fundamentals-v3/aspnet-core-fundamentals-v3/Controllers/AboutController.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
 namespace aspnet_core_fundamentals_v3.Controllers
 {
+    [Route("about")]
     public class AboutController
     {
+        [Route("phone")]
         public string phone()
         {
             return "555-867-5309";
         }
 
+        [Route("address")]
         public string address()
         { return "USA"; }
+
+        [Route("")]
+        public string Index()
+        {
+            return "Hello, there!";
+        }
     }
 }
