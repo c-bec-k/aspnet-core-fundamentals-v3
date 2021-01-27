a1032f9580974881e4501d9b3eea0a48070250ca
cbeck <cruthenbeck@gmail.com>
2021-01-26 19:14:41 -0500

    Step032: custom identity dbcontext & configuration

--- aspnet-core-fundamentals-v3/SimpleCrm.Web/Startup.cs
+++ aspnet-core-fundamentals-v3/SimpleCrm.Web/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,9 @@ namespace SimpleCrm.Web
             services.AddSingleton<IGreeter, ConfigurationGreeter>();
             services.AddScoped<ICustomerData, SqlCustomerData>();
             services.AddDbContext<SimpleCrmDbContext>(options => options.UseNpgsql(Config.GetConnectionString("SimpleCrmConnection")));
+            services.AddDbContext<CrmIdentityDbContext>(options => options.UseNpgsql(Config.GetConnectionString("SimpleCrmConnection")));
+            services.AddIdentity<CrmUser, IdentityRole>()
+                    .AddEntityFrameworkStores<CrmIdentityDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +52,10 @@ namespace SimpleCrm.Web
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
---
+++ aspnet-core-fundamentals-v3/SimpleCrm/CrmUser.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace SimpleCrm
+{
+  public class CrmUser : IdentityUser
+  {
+
+    [MaxLength(256)]
+    public string DisplayName { get; set; }
+    
+  }
+}
