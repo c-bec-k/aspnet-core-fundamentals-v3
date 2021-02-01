2dc1760b2b481bbf847259075089c8c097533c72
cbeck <cruthenbeck@gmail.com>
2021-02-01 11:59:52 -0500

    Step034: account registration started

---
+++ aspnet-core-fundamentals-v3/SimpleCrm.Web/Models/Account/RegisterUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.Web.Models.Account
+{
+  public class RegisterUserViewModel
+  {
+    [Required(), DisplayName("Email"), MaxLength(256), DataType(DataType.EmailAddress)]
+    public string UserName { get; set; }
+
+    [Required(), MaxLength(256), DataType(DataType.Text), DisplayName("Name")]
+    public string DisplayName { get; set; }
+
+    [Required(), DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required(), DataType(DataType.Password), Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; }
+  }
+}
