0e24ed645767ab25b62b5cdf0fe30bf5f7ab32a9
cbeck <cruthenbeck@gmail.com>
2021-02-01 12:44:36 -0500

    Step035: added user account registration

--- aspnet-core-fundamentals-v3/SimpleCrm.Web/Controllers/AccountController.cs
+++ aspnet-core-fundamentals-v3/SimpleCrm.Web/Controllers/AccountController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using SimpleCrm.Web.Models.Account;
 
 namespace SimpleCrm.Web.Controllers
 {
   public class AccountController : Controller
   {
+
+    private readonly UserManager<CrmUser> userManager;
+    private readonly SignInManager<CrmUser> signInManager;
+    public AccountController(UserManager<CrmUser> userManager, SignInManager<CrmUser> signInManager)
+    {
+      this.userManager = userManager;
+      this.signInManager = signInManager;
+    }
+
     [HttpGet()]
     public IActionResult Register()
     {
@@ -12,9 +23,32 @@ namespace SimpleCrm.Web.Controllers
     }
 
     [HttpPost(), ValidateAntiForgeryToken()]
-    public IActionResult Register(RegisterUserViewModel model)
+    public async Task<IActionResult> Register(RegisterUserViewModel model)
     {
-      return NoContent();
+      if (ModelState.IsValid)
+      {
+          var user = new CrmUser
+        {
+          UserName = model.UserName,
+          DisplayName = model.DisplayName,
+          Email = model.UserName
+        };
+
+        var createResult = await this.userManager.CreateAsync(user, model.Password);
+
+        if (createResult.Succeeded)
+        {
+          await signInManager.SignInAsync(user, isPersistent: false);
+          return RedirectToAction("Index", "Home");
+        }
+        foreach (var result in createResult.Errors)
+        {
+          ModelState.AddModelError("", result.Description);
+        }
+      }
+
+      return View();
+
     }
   }
 }
