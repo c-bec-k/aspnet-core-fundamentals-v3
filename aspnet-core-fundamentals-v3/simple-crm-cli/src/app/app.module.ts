1a2681114e9d7ad7f47c97e3b537ba89a7a6064f
c-bec-k <cruthenbeck@gmail.com>
2021-05-11 15:38:45 -0400

    66b: Better icons

--- aspnet-core-fundamentals-v3/simple-crm-cli/src/app/app.module.ts
+++ aspnet-core-fundamentals-v3/simple-crm-cli/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { CustomerModule } from './customer/customer.module';
 import { AppIconsService } from './app-icons.service';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 
 
@@ -26,6 +27,7 @@ import { AppIconsService } from './app-icons.service';
     MatIconModule,
     MatSidenavModule,
     MatListModule,
+    MatDatepickerModule,
   ],
   providers: [AppIconsService],
   bootstrap: [AppComponent]
