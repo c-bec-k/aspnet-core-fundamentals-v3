0f1667262c811b03e4284758e87dda7e3e585362
c-bec-k <cruthenbeck@gmail.com>
2021-06-01 10:37:26 -0400

    100: expanded customer model

--- aspnet-core-fundamentals-v3/SimpleCrm/Customer.cs
+++ aspnet-core-fundamentals-v3/SimpleCrm/Customer.cs
@@ -15,10 +15,31 @@ namespace SimpleCrm
         [Required()]
         public string LastName { get; set; }
 
-        [MinLength(7), MaxLength(12)]
+        [MinLength(1), MaxLength(20)]
         public string PhoneNumber { get; set; }
 
         public bool OptInNewsletter { get; set; }
         public CustomerType Type { get; set; }
+
+        [Required()]
+        public string EmailAddress { get; set; }
+
+        public InteractionMethod PeferredContactMethod { get; set; }
+        public CustomerStatus StatusCode { get; set; }
+    }
+
+    public enum InteractionMethod
+    {
+        None = 0,
+        Email = 1,
+        Phone = 2
+    }
+
+    public enum CustomerStatus
+    {
+        not_interested = 0,
+        customer = 1,
+        prospect = 2,
+        unknown = 3
     }
 }
