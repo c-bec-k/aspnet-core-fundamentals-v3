0b80b09baaa34b0923f8af6ece947d3665eae1e2
c-bec-k <cruthenbeck@gmail.com>
2021-09-21 19:51:51 -0400

    116: create AuthController and CredentialsViewModel

---
+++ aspnet-core-fundamentals-v3/SimpleCrm.WebApi/ApiControllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.WebApi.Models;
+
+namespace SimpleCrm.WebApi.ApiControllers
+{
+  public class AuthController
+  {
+    [HttpPost("login")]
+    public async Task<IActionResult> Post([FromBody] CredentialsViewModel credentials)
+    { // TODO: create a CredentialsViewModel class in the next assignment
+      if (!ModelState.IsValid)
+      {
+        return UnprocessableEntity(ModelState);
+      }
+
+      // TODO: add Authenticate method (see lesson below)
+      var user = await Authenticate(credentials.EmailAddress, credentials.Password);
+      if (user == null)
+      {
+        return UnprocessableEntity("Invalid username or password");
+      }
+
+      // TODO: add GetUserData method (see lesson below)
+      var userModel = await GetUserData(user);
+    }
+    // returns a UserSummaryViewModel containing a JWT and other user info
+    return Ok(userModel);
+  }
+}
---
+++ aspnet-core-fundamentals-v3/SimpleCrm.WebApi/Models/CredentialsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.WebApi.Models
+{
+  public class CredentialsViewModel
+  {
+    [MaxLength(30), Required]
+    public string EmailAddress { get; set; }
+
+
+    [MaxLength(72), Required]
+    public string Password { get; set; }
+  }
+}
