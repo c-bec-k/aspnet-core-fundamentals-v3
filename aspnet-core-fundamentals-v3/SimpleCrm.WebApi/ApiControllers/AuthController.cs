a87aa7200dfc44b02f0b4b12ea4297d51816cdd7
c-bec-k <cruthenbeck@gmail.com>
2021-11-23 21:04:38 -0500

    119: complete the OAuth JWT login flow in API

--- aspnet-core-fundamentals-v3/SimpleCrm.WebApi/ApiControllers/AuthController.cs
+++ aspnet-core-fundamentals-v3/SimpleCrm.WebApi/ApiControllers/AuthController.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using SimpleCrm.WebApi.Auth;
 using SimpleCrm.WebApi.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace SimpleCrm.WebApi.ApiControllers
 {
@@ -13,20 +17,100 @@ namespace SimpleCrm.WebApi.ApiControllers
   {
     private readonly UserManager<CrmUser> _userManager;
     private readonly IJwtFactory _jwtFactory;
+    private readonly MSAuthSettings _microsoftAuthSettings;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<AuthController> _logger;
 
-    public AuthController(UserManager<CrmUser> userManager, IJwtFactory jwtFactory)
+    public AuthController(UserManager<CrmUser> userManager, IJwtFactory jwtFactory,
+    MSAuthSettings microsoftAuthSettings, IConfiguration configuration, ILogger<AuthController> logger)
     {
+      this._configuration = configuration;
+      this._microsoftAuthSettings = microsoftAuthSettings;
       this._userManager = userManager;
       this._jwtFactory = jwtFactory;
+      this._logger = logger;
     }
 
-    //[HttpPost("external/microsoft")]
-    //public async Task<IActionResult> PostMicrosoft([FromBody] MicrosoftAuthViewModel model);
+
+    // 
+    // Step 6: Enable OAuth - JWT Login with Microsoft
+    // 
+
+    [HttpGet("external/microsoft")]
+    public IActionResult GetMicrosoft()
+    {
+      return Ok(new
+      {
+        client_id = _microsoftAuthSettings.ClientId,
+        scope = "https://graph.microsoft.com/user.read",
+        state = ""
+      });
+    }
+
+    [HttpPost("external/microsoft")]
+    public async Task<IActionResult> PostMicrosoft([FromBody] MicrosoftAuthViewModel model)
+    {
+      var verifier = new MicrosoftAuthVerifier<AuthController>(_microsoftAuthSettings, _configuration["HttpHost"] + (model.BaseHref ?? "/"), _logger);
+      var profile = await verifier.AcquireUser(model.AccessToken);
+
+      // TODO: validate the 'profile' object is successful, and email address is included
+
+      if (profile.IsSuccessful == false)
+      {
+        return StatusCode(StatusCodes.Status400BadRequest, profile.Error.Message);
+      }
+
+      if (String.IsNullOrWhiteSpace(profile.Mail))
+      {
+        return StatusCode(StatusCodes.Status403Forbidden, "Email address is required");
+      }
+
+      var UserLoginInfo = new UserLoginInfo("Microsoft", profile.Id, profile.DisplayName);
+
+      var user = await _userManager.FindByEmailAsync(profile.Mail);
+      if (user == null)
+      {
+
+        user = new CrmUser
+        {
+          DisplayName = profile.DisplayName,
+          Email = profile.Mail,
+          PhoneNumber = profile.MobilePhone,
+          UserName = profile.Mail
+        };
+
+        await _userManager.CreateAsync(user);
+      }
+
+      var userModel = await GetUserData(user);
+      return Ok(userModel);
+    }
+
+
+    // 
+    // Step 7 Allow clients to refresh login info + JWT
+    // 
+
+    [Authorize(Policy = "ApiUser")]
+    [HttpPost("verify")] // POST api/auth/verify
+    public async Task<IActionResult> Verify()
+    {
+      if (User.Identity.IsAuthenticated)
+      {
+        var user = await _userManager.FindByEmailAsync(User.Identity.Name);
+        if (user == null)
+          return Forbid();
+        var userModel = await GetUserData(user);
+        return new ObjectResult(userModel);
+      }
+
+      return Forbid();
+    }
 
 
     [HttpPost("login")]
     public async Task<IActionResult> Post([FromBody] CredentialsViewModel credentials)
-    { 
+    {
       if (!ModelState.IsValid)
       {
         return UnprocessableEntity(ModelState);
@@ -38,10 +122,10 @@ namespace SimpleCrm.WebApi.ApiControllers
         return UnprocessableEntity("Invalid username or password");
       }
 
-      
+
       var userModel = await GetUserData(user);
 
-      
+
       return Ok(userModel);
     }
 
---
+++ aspnet-core-fundamentals-v3/SimpleCrm.WebApi/Auth/MicrosoftAuthViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SimpleCrm.WebApi.Auth
+{
+  public class MicrosoftAuthViewModel
+  {
+    public string AccessToken { get; set; }
+    public string State { get; set; }
+    public string BaseHref { get; set; }
+  }
+}
