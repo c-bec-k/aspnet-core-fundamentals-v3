18b78d9fb45e8a3964d6f00cc5db318850c5c8a6
c-bec-k <cruthenbeck@gmail.com>
2022-03-08 20:20:24 -0500

    152: create authenticated guard

--- aspnet-core-fundamentals-v3/simple-crm-cli/src/app/customer/customer-routing.module.ts
+++ aspnet-core-fundamentals-v3/simple-crm-cli/src/app/customer/customer-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticatedGuard } from '../account/authenticated.guard';
 import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 
 const routes: Routes = [
   {
     path: 'customers',
+    canActivate: [AuthenticatedGuard],
     pathMatch: 'full',
     component: CustomerListPageComponent
   },
   {
     path: 'customer/:id',
+    canActivate: [AuthenticatedGuard],
     pathMatch: 'full',
     component: CustomerDetailComponent
-  }
+  },
 ];
 
 @NgModule({
