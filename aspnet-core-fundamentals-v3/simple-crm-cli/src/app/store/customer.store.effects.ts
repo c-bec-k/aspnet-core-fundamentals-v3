2fe926b6b26cebd275c21cfccaea227caac9fa86
c-bec-k <cruthenbeck@gmail.com>
2022-06-17 13:52:15 -0400

    158: register ngrx effects

--- aspnet-core-fundamentals-v3/simple-crm-cli/src/app/store/customer.store.effects.ts
+++ aspnet-core-fundamentals-v3/simple-crm-cli/src/app/store/customer.store.effects.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { switchMap } from 'rxjs';
 import { CustomerService } from '../customer/customer.service';
 
 @Injectable()
 
 export class CustomerStoreEffects {
-  constructor( private actions$: Actions, private custSvc: CustomerService) {
+  constructor( private actions$: Actions, private custSvc: CustomerService) { }
 
-  }
+  searchCustomers$ = createEffect( () => this.actions$.pipe(
+    ofType(searchCustomersAction),
+    switchMap({criteria}) => this.custSvc.search(criteria.term).pipe(
+      map(
+        data => searchCustomersCompleteAction({result: data});
+      )
+    )
+  ));
 }
